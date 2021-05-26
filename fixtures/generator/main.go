f9d6711edc0ce06cb589707ff25a03433efbab4b
Samlet Wu <xiaofei.wu@gmail.com>
2021-05-26 16:14:35 +0800

    add generator for entity

---
+++ fixtures/generator/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+	metagen "github.com/samlet/petrel/meta"
+	"github.com/urfave/cli/v2"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+/**
+$ just run generator -s type -e Person
+*/
+
+func main() {
+	prompt := color.New(color.FgYellow).PrintfFunc()
+	imp := color.New(color.FgGreen).SprintFunc()
+
+	var service string
+
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "service",
+				Aliases:     []string{"s"},
+				Value:       "find",
+				Usage:       "service to execute",
+				Destination: &service,
+			},
+			&cli.IntFlag{Name: "size", Aliases: []string{"m"}, Value: 20},
+			&cli.StringFlag{Name: "entity", Aliases: []string{"e"},
+				Usage: "entity name",
+				Value: "WebSite"},
+		},
+		Action: func(c *cli.Context) error {
+			act := "_none_"
+			if c.NArg() > 0 {
+				act = c.Args().Get(0)
+				prompt(".. act is %s\n", act)
+			}
+			switch service {
+			case "type":
+				entity := c.String("entity")
+				if entity != "" {
+					meta, err := metagen.GetEntityMeta(entity)
+					if err != nil {
+						panic(err)
+					}
+					tplData, err := ioutil.ReadFile("incls/entity_def.tmpl")
+					if err != nil {
+						panic(err)
+					}
+					result := metagen.GenEntity(&meta.Data.Entity, string(tplData))
+					println(result)
+				}
+			case "env":
+				fmt.Println("GOPATH:", os.Getenv("GOPATH"))
+			default:
+				fmt.Printf("Cannot to execute %s.\n", imp(service))
+			}
+
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
